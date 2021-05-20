2683fd7e0fc2ba68036cdbe862d59ed61a5db139
Amitkumar Mishra <mr.mash@gmail.com>
2021-05-20 11:39:27 +0530

    finished

---
+++ src/components/TransactionsList.jsx
@@ -0,0 +1,18 @@
+import { useContext, useEffect } from 'react'
+import CryptoContext from '../contexts/CryptoContext'
+import Transaction from './Transaction'
+
+export default function TransactionsList() {
+    let { transactions } = useContext(CryptoContext)
+    console.log(transactions)
+    useEffect(() => { }, [transactions])
+    return (
+        <div className="transactions-list">
+            <h1>Transactions</h1>
+            <div className="list">
+                {!transactions.length && <p>No transactions yet...</p>}
+                {transactions.length > 0 && transactions.map((transaction, idx) => <Transaction value={transaction} key={idx} />)}
+            </div>
+        </div>
+    )
+}
